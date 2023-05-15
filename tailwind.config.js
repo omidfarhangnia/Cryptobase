15f8f042e6287cbeb9f00b51df1996d91fb4f214
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-05-15 23:22:33 +0330

    adding react-icon and making tailwind custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dollor: "#659C44",
+        coin: "#F2A900",
+        platinum: "#E6E6E6",
+        darkBlue: "#234058"
+      },
+    },
+    screens: {
+      'ml': '896px',
+      // medla => something between medium and large
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }
